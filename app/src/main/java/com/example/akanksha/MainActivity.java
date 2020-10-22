d60f201131b3aeb277ce40d710b411401f1c5eb0
Ian Campbell <ian.campbell.92@gmail.com>
2020-10-21 20:20:25 -0600

    cleaning up code, adding comments

--- app/src/main/java/com/example/akanksha/MainActivity.java
+++ app/src/main/java/com/example/akanksha/MainActivity.java
@@ -12,10 +12,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // modulo is not well defined for negative numbers in java. this makes it wrap around
+    // appropriately
     int mod(int n, int m) {
         return ((n % m) + m) % m;
     }
-    int currentScreen = 0;
     int[] buttonIds = new int[]{
                 R.id.button,
                 R.id.button2,
@@ -28,42 +29,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 R.id.button9,
                 R.id.button10
         };
-
-    Map<Integer,Integer> buttonToScreen = new HashMap<>();
-    Map<Integer,int[]> screenToButtons = new HashMap<>();
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        int[] viewIds = new int[]{
+    int[] viewIds = new int[]{
                 R.layout.activity_main,
                 R.layout.activity_main1,
                 R.layout.activity_main2,
                 R.layout.activity_main3,
                 R.layout.activity_main4,
         };
+
+    Map<Integer,Integer> buttonToScreen = new HashMap<>();
+    Map<Integer,int[]> screenToButtons = new HashMap<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        set up the hashmap relationships here.
+//        this could also be done manually.
         for (int i = 0; i < buttonIds.length; i++){
             int viewIndex = (int) Math.floor(i / 2);
-            int nextViewIndex  = (i%2 == 0) ? mod(viewIndex-1, 5) : ((viewIndex+1) % 5);
+            int adder = (i%2 == 0) ? 1:-1;
+            int nextViewIndex  = mod(viewIndex + adder, 5);
             buttonToScreen.put(buttonIds[i], viewIds[nextViewIndex]);
         }
         for (int i = 1; i < buttonIds.length; i+=2){
             int viewIndex = (int) Math.floor(i / 2);
             screenToButtons.put(viewIds[viewIndex], new int[] {buttonIds[i-1], buttonIds[i]});
         }
-
-        setContentView(R.layout.activity_main);
-        int[] buttonIds  =screenToButtons.get(R.layout.activity_main);
-        findViewById(buttonIds[0]).setOnClickListener(this);
-        findViewById(buttonIds[1]).setOnClickListener(this);
+        updateView(R.layout.activity_main);
 
     }
 
+    private void updateView(int viewId){
+        setContentView(viewId);
+        int[] buttonIds = screenToButtons.get(viewId);
+        findViewById(buttonIds[0]).setOnClickListener(this);
+        findViewById(buttonIds[1]).setOnClickListener(this);
+    }
     @Override
     public void onClick(View button) {
         int viewId = buttonToScreen.get(button.getId());
-        setContentView(viewId);
-        int[] buttonIds  =screenToButtons.get(viewId);
-        findViewById(buttonIds[0]).setOnClickListener(this);
-        findViewById(buttonIds[1]).setOnClickListener(this);
+        updateView(viewId);
     }
 }
